ace68f8cdb4325831d2d7b9ddbdf0f0f2f63f03b
MSNP1381 <mohamandnematpoor@gmail.com>
2024-07-24 12:11:19 +0330

    small fix

--- agent.py
+++ agent.py
@@ -137,7 +137,7 @@ if __name__ == "__main__":
     config = {"callbacks": [langfuse_handler]}
     solver = KaggleProblemSolver(config, proxy)
     graph=solver.compile()
-    t=graph.get_graph(xray=2).draw_mermaid()
+    t=graph.get_graph(xray=2).draw_mermaid_png(output_file_path="agentVis.png")
     with open("x.txt",'w') as f:
         f.write(t)
     exit()
